d7bf44a2d15438481df0ee5609077c16211b4579
jojiapp <72853422+jojiapp@users.noreply.github.com>
2022-05-29 08:09:33 +0000

    Deploying to gh-pages from  @ ef091379014b1d05843754f836457256de2d4c55 🚀

---
+++ _next/static/-MLce5bL2kFuwL3NjrokN/_ssgManifest.js
@@ -0,0 +1,1 @@
+self.__SSG_MANIFEST=new Set(["\u002Fblog","\u002F","\u002Ftags","\u002Ftil","\u002Fabout","\u002Fblog\u002Fpage\u002F[page]","\u002Ftags\u002F[tag]","\u002Ftil\u002Fpage\u002F[page]","\u002Fblog\u002F[...slug]","\u002Ftil\u002F[...slug]"]);self.__SSG_MANIFEST_CB&&self.__SSG_MANIFEST_CB()
